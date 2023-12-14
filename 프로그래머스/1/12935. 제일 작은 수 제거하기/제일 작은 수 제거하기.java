43038b2c40b5cab724b76bd314e566c67361f07b
thkim610 <112153004+thkim610@users.noreply.github.com>
2023-12-14 15:20:20 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.05 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -30,3 +30,28 @@ class Solution {
         return answer;
     }
 }
+       //배열의 순서를 고려하지 않아서 생긴 오답.
+/*       //배열 내림차순 정렬 (Arrays.sort(배열, Collections.reverseOrder()))
+        // 기본형 타입을 래퍼클래스 배열로 변환 후 정렬
+        Integer[] arr2 = new Integer[arr.length];
+        
+        for(int i=0; i<arr.length; i++){
+            arr2[i] = Integer.valueOf(arr[i]);
+        }
+        //내림차순으로 정렬
+        Arrays.sort(arr2, Collections.reverseOrder());
+        
+        //배열의 길이가 1이면, -1을 할당하고 반환
+        if(arr2.length==1){
+            answer = new int[1];
+            answer[0] = -1;
+        }else{
+        //배열의 맨 마지막 값을 제외한 나머지 값을 담은 배열 반환
+        //=> 배열을 내림차순으로 정렬하였기 때문에 배열의 마지막 값을 가장 작은 값임.
+            answer = new int[arr2.length-1]; //배열의 길이 = 가장 작은 값을 뺀 길이
+            
+            for(int i=0; i<arr2.length-1; i++){
+                answer[i] = arr2[i];
+            }*/
+
+
