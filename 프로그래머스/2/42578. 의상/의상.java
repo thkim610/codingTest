17c6f61e6c277e6bc6a214ee8c85c183b6a8b55a
thkim610 <112153004+thkim610@users.noreply.github.com>
2024-02-27 14:27:53 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,27 +2,30 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
-        
-        HashMap<String, Integer> type = new HashMap<>();
-        //옷의 종류 별 개수를 구한다.
-        for( String[] cloth : clothes ){
-            
-            //이미 있는 경우 값을 1 증가
-            if( type.containsKey(cloth[1]) ){
-                type.put(cloth[1], type.get(cloth[1])+1);
-            } else{
-            	//없는 경우 값을 1로 저장한다.
-                type.put(cloth[1], 1);
-            }
+		
+		
+		HashMap<String, Integer> clothesMap = new HashMap<>();
+		
+		//종류와 옷 개수를 매핑한 hashMap 생성.
+		for(int i=0; i<clothes.length; i++) {
+            //종류가 없으면, 맵에 종류 1개를 기본으로 저장. 
+			if(!clothesMap.containsKey(clothes[i][1])) {
+				clothesMap.put(clothes[i][1], 1);				
+			}else { //있으면, 개수를 +1 증가.
+				clothesMap.put(clothes[i][1], clothesMap.get(clothes[i][1])+1);
+			}
+			
+		}
+		
+		
+		//공식 : (a+1)(b+1)...(해당 옷 종류의 개수 +1)-1
+		for( String key : clothesMap.keySet()){
+            answer *= clothesMap.get(key)+1;
         }
 		
-        //(a + 1)(b + 1)...
-        for( String key : type.keySet()){
-            answer *= type.get(key)+1;
-        }
-        // -1
-        answer -= 1;
+		answer -= 1;
         
         return answer;
+		
     }
 }
