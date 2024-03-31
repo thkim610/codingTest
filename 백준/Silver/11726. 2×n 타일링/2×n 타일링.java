a26add40b4f536ee98a26aac7f86c84c1d1bb4a6
thkim610 <112153004+thkim610@users.noreply.github.com>
2024-03-31 22:17:53 +0900

    [Silver III] Title: 2×n 타일링, Time: 208 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	static long mod = 10007;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		int N = sc.nextInt();
+		
+		long D[] = new long[1001];
+		D[1] = 1;
+		D[2] = 2;
+		
+		for(int i = 3; i <= N; i++) {
+			D[i] = (D[i - 1] + D[i - 2]) % mod;
+		}
+		
+		System.out.println(D[N]);
+
+	}
+
+}
