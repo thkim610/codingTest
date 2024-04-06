9fa51d109edc1fe8bdad026e159d3fc3d8924cf2
thkim610 <112153004+thkim610@users.noreply.github.com>
2024-04-06 18:12:24 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        int even = 0;
+        int odd = 0;
+        
+        for(int num : num_list){
+            if(num % 2 == 0){
+                even++;
+                answer[0] = even;
+            }else{
+                odd++;
+                answer[1] = odd;
+            }
+        }
+        
+        return answer;
+    }
+}
